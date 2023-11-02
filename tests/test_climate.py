475ef8be03a05b3225620040cb5eaa5fd6273fb2
Tony Roberts <tony@pyxll.com>
2023-11-02 12:16:11 +0000

    Fix typo wundersmart to wundasmart

--- tests/test_climate.py
+++ tests/test_climate.py
@@ -1,6 +1,6 @@
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 from pytest_homeassistant_custom_component.common import load_fixture
-from custom_components.wundersmart.const import DOMAIN
+from custom_components.wundasmart.const import DOMAIN
 from unittest.mock import patch
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 from homeassistant.core import HomeAssistant
@@ -15,7 +15,7 @@ async def test_climate(hass: HomeAssistant, config):
 
     # Test setup of climate entity fetches initial state
     data = json.loads(load_fixture("test_get_devices1.json"))
-    with patch("custom_components.wundersmart.get_devices", return_value=data):
+    with patch("custom_components.wundasmart.get_devices", return_value=data):
         await hass.config_entries.async_setup(entry.entry_id)
         await hass.async_block_till_done()
 
@@ -32,7 +32,7 @@ async def test_climate(hass: HomeAssistant, config):
 
     # Test refreshing coordinator updates entity state
     data = json.loads(load_fixture("test_get_devices2.json"))
-    with patch("custom_components.wundersmart.get_devices", return_value=data):
+    with patch("custom_components.wundasmart.get_devices", return_value=data):
         await coordinator.async_refresh()
         await hass.async_block_till_done()
 
@@ -47,8 +47,8 @@ async def test_climate(hass: HomeAssistant, config):
     # Test setting temperature works
     data = json.loads(load_fixture("test_get_devices3.json"))
     tdata = json.loads(load_fixture("test_set_temperature.json"))
-    with patch("custom_components.wundersmart.get_devices", return_value=data), \
-            patch("custom_components.wundersmart.climate.send_command", return_value=tdata) as mock:
+    with patch("custom_components.wundasmart.get_devices", return_value=data), \
+            patch("custom_components.wundasmart.climate.send_command", return_value=tdata) as mock:
         await hass.services.async_call("climate", "set_temperature", {
             "entity_id": "climate.test_room",
             "temperature": 20
--- tests/test_water_heater.py
+++ tests/test_water_heater.py
@@ -1,7 +1,7 @@
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 from pytest_homeassistant_custom_component.common import load_fixture
-from custom_components.wundersmart.const import DOMAIN
-from custom_components.wundersmart.water_heater import STATE_AUTO_ON, STATE_AUTO_OFF, STATE_BOOST_ON
+from custom_components.wundasmart.const import DOMAIN
+from custom_components.wundasmart.water_heater import STATE_AUTO_ON, STATE_AUTO_OFF, STATE_BOOST_ON
 from unittest.mock import patch
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 from homeassistant.core import HomeAssistant
@@ -15,7 +15,7 @@ async def test_water_header(hass: HomeAssistant, config):
 
     # Test setup of water heater entity fetches initial state
     data = json.loads(load_fixture("test_get_devices1.json"))
-    with patch("custom_components.wundersmart.get_devices", return_value=data):
+    with patch("custom_components.wundasmart.get_devices", return_value=data):
         await hass.config_entries.async_setup(entry.entry_id)
         await hass.async_block_till_done()
 
@@ -28,7 +28,7 @@ async def test_water_header(hass: HomeAssistant, config):
     assert coordinator
 
     data = json.loads(load_fixture("test_get_devices2.json"))
-    with patch("custom_components.wundersmart.get_devices", return_value=data):
+    with patch("custom_components.wundasmart.get_devices", return_value=data):
         await coordinator.async_refresh()
         await hass.async_block_till_done()
 
@@ -38,7 +38,7 @@ async def test_water_header(hass: HomeAssistant, config):
         assert state.state == STATE_BOOST_ON
 
     data = json.loads(load_fixture("test_get_devices3.json"))
-    with patch("custom_components.wundersmart.get_devices", return_value=data):
+    with patch("custom_components.wundasmart.get_devices", return_value=data):
         await coordinator.async_refresh()
         await hass.async_block_till_done()
 
