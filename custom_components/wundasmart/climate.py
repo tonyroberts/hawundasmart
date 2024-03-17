29402597fb92050d10963d717d45f2ac9ed44846
Dan Elphick <dabuek@gmail.com>
2024-03-17 14:31:08 +0000

    Remove TURN_OFF and TURN_ON flags

--- custom_components/wundasmart/climate.py
+++ custom_components/wundasmart/climate.py
@@ -118,10 +118,7 @@ class Device(CoordinatorEntity[WundasmartDataUpdateCoordinator], ClimateEntity):
         # https://developers.home-assistant.io/blog/2024/01/24/climate-climateentityfeatures-expanded/
         self._enable_turn_on_off_backwards_compatibility = False
         self._attr_supported_features = (
-            ClimateEntityFeature.TARGET_TEMPERATURE
-            | ClimateEntityFeature.PRESET_MODE
-            | ClimateEntityFeature.TURN_OFF
-            | ClimateEntityFeature.TURN_ON
+            ClimateEntityFeature.TARGET_TEMPERATURE | ClimateEntityFeature.PRESET_MODE
         )
         self._attr_current_temperature = None
         self._attr_target_temperature = None
